562314032c932aa9ff1cd4cb7e0b485566890bc5
Sayo Law <shayolaw@gmail.com>
2024-12-04 01:36:40 -0500

    finished 2 spots

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,20 +10,71 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+let user = users.filter((user)=>user.username===username && user.password===password)
+if(user.length>0){
+    return true;
+}
+return false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const {username,password} = req.body;
 
+    if(!username || !password){
+      res.send("Error Logging in")
+    }
+    if(authenticatedUser(username, password)) {
+      let accessToken = jwt.sign({
+          data: password
+      }, 'access', {expiresIn: 60*60});
+      req.session.authorization = {
+          accessToken, username
+      }
+      return res.status(200).send("User successfully logged in");
+    }
+    else{
+    return res.status(208).json({message: "Invalid Login"});
+    }
+  });
+const checkUserReview = (book,user) =>{
+    if(book.reviews[user]){
+        return true;
+    }
+    return false;
+
+}
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    
+    const isbn = req.params.isbn;
+    const user = req.session.authorization['username'];
+    const review = req.body.review;
+    let book = books[isbn];
+    
+    if(checkUserReview(book,user)){
+        book.reviews[user] = review;
+      
+    }
+    else{
+        book.reviews[user] = review;
+       
+    }
+
+    books[isbn] = book; 
+    res.send(books);
 });
+regd_users.delete("/auth/review/:isbn",(req,res)=>{
+    const user = req.session.authorization['username'];
+    const review = req.body.review;
+    const isbn = req.params.isbn
+    let book = books[isbn];
+    if(checkUserReview(book,user)){
+        delete book.reviews[user];
+    }
+    books[isbn] = book;
+    res.send(books);
+})
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
